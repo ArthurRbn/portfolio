75f434c78fa8f9df96006b371207d293170dddeb
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-19 00:11:01 +0200

    fix: pass 403 and 404s to the origin

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -22,20 +22,33 @@ resource "aws_cloudfront_distribution" "this" {
 
   default_cache_behavior {
     target_origin_id = local.s3_origin_id
-    viewer_protocol_policy = "redirect-to-https"
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods  = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
 
     forwarded_values {
       query_string = true
+
       cookies {
         forward = "all"
       }
     }
 
-    min_ttl     = 0
-    default_ttl = 0
-    max_ttl     = 0
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
   }
 
   restrictions {
