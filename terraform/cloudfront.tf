7da23a42223111de47e405bafff50353884f8633
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-18 22:17:30 +0200

    fix: configure cloudfront access to the S3 bucket

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -3,38 +3,37 @@ locals {
   s3_domain_name = "${var.s3_name}.s3-website-${var.region}.amazonaws.com"
 }
 
+resource "aws_cloudfront_origin_access_identity" "this" {
+  comment = "OAI for accessing S3 bucket ${var.s3_name}"
+}
+
 resource "aws_cloudfront_distribution" "this" {
   enabled = true
 
   origin {
-    origin_id                = local.s3_origin_id
-    domain_name              = local.s3_domain_name
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
+    origin_id   = local.s3_origin_id
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
     }
   }
 
   default_cache_behavior {
-
     target_origin_id = local.s3_origin_id
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
 
     forwarded_values {
       query_string = true
-
       cookies {
         forward = "all"
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
+    min_ttl     = 0
+    default_ttl = 0
+    max_ttl     = 0
   }
 
   restrictions {
@@ -48,5 +47,4 @@ resource "aws_cloudfront_distribution" "this" {
   }
 
   price_class = "PriceClass_200"
-
 }
